e5ae1c62f49a3dd441e7d8d88b0e6a8a59770273
Ida Wang <iwang2@stuy.edu>
2017-04-13 00:33:32 -0400

    finished writing methods, need to be implemented into Layout and/or put into wrapper functions

--- Complex.java
+++ Complex.java
@@ -46,13 +46,53 @@ public class Complex{
 	return ans;
     }
     
+    // [r, theta] ^ 1/root --> [r ^ 1/root, theta / root]
+    private static double[] root1(double[] in, double root){
+	double r = in[0], theta = in[1];
+	r = Math.pow(r, 1/root);
+	theta = theta / root;
+	double[] ans = {r, theta};
+	return ans;
+    }
+
+    // theta && root --> array of every combination of 
+    private static double[] addRoots(double theta, double root){
+	double[] ans = new double[root];
+	double increment = 2 * Math.PI / root;
+	for(int i = 0; i < root; i++){
+	    ans[i] = increment * i + theta;
+	}
+	for(int i = 0 ; i < root; i++){
+	    while(ans[i] > 2 * Math.PI){
+		ans[i] -= 2 * Math.PI;
+	    }
+	}
+	return ans;
+    }
+    
+    // [r, theta] --> [a, b]
+    private static double[] toRect(double[] polar){
+	double r = polar[0], theta = polar[1];
+	double a = r * Math.cos(theta), b = r * Math.sin(theta);
+	double[] ans = {a, b};
+	return ans;
+    }
+    
     public static void main(String[] args){
-	double[] a = values("3 + 3i");
+	double[] a = values("0 + 3i");
 	System.out.println(a[0]);
 	System.out.println(a[1]);
+	
 	double[] a1 = toPolar(a);
 	a1 = power(a1, 2);
 	System.out.println(a1[0]);
 	System.out.println(a1[1]);
+	a1 = root1(a1, 2);
+	System.out.println(a1[0]);
+	System.out.println(a1[1]);
+
+	double[] a2 = toRect(a1);
+	System.out.println(a2[0]);
+	System.out.println(a2[1]);
     }
 }
